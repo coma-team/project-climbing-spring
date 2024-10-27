d3ea2ae4b770d52d1825b085f63f27cef50a86ef
junyeol <junyeoli75@gmail.com>
2024-10-27 17:42:27 +0900

    [로직 수정] - junyeol 241027 Controller BoardController.java, CommunityController.java, CrewBattleController.java, CrewController.java, MainController.java, ReplyController.java, ReservationManagementController.java

--- src/main/java/com/coma/app/view/admin/ReservationManagementController.java
+++ src/main/java/com/coma/app/view/admin/ReservationManagementController.java
@@ -2,11 +2,19 @@ package com.coma.app.view.admin;
 
 import com.coma.app.biz.battle.BattleDTO;
 import com.coma.app.biz.board.BoardDTO;
+import com.coma.app.biz.crew_board.Crew_boardDTO;
 import com.coma.app.biz.gym.GymDTO;
 import com.coma.app.biz.member.MemberDTO;
 import com.coma.app.biz.reservation.ReservationDTO;
+import com.coma.app.biz.reservation.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Autowired
+ReservationService reservationService;
 
 public class ReservationAdminController {
 
@@ -16,10 +24,9 @@ public class ReservationAdminController {
     }
 
     // 예약 관리
-    public String reservationManagement(Model model, MemberDTO memberDTO, GymDTO gymDTO, BoardDTO boardDTO, BattleDTO battleDTO, ReservationDTO reservationDTO) {
+    public String reservationManagement(Model model, MemberDTO memberDTO,  ReservationDTO reservationDTO) {
 
 		/*
-		차트js 빼기로 합의봤음 ㅎㅎ
 
 		예약자명
 		예약한 암벽장 이름
@@ -29,10 +36,31 @@ public class ReservationAdminController {
 
 		페이지네이션
 
-
 		 */
+        int pageNum = reservationDTO.getPage();//요거 필요
+        int boardSize = 10; // 한 페이지에 표시할 게시글 수 설정
+        int minBoard = 1; // 최소 게시글 수 초기화
 
-        return null;
+        if (pageNum <= 0) { // 페이지가 0일 때 (npe방지)
+            pageNum = 1;
+        }
+
+        minBoard = ((pageNum - 1) * boardSize); // 최소 게시글 번호 계산
+        int listNum = 0; // 게시글 총 개수를 저장할 변수 초기화
+
+        reservationDTO.setPage(minBoard);
+
+        listNum = reservationDTO.getBoard_total();
+
+        //=========페이지네이션==============
+
+       List<ReservationDTO> datas = reservationService.selectAll(reservationDTO);
+model.addAttribute("datas", datas);
+model.addAttribute("total", listNum);
+model.addAttribute("page", pageNum);
+
+
+        return "admin/reservationManagement";
     }
 
 
